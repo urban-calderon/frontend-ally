573fc2e5526ace4e17f0cb5dd99aace48dbe4f67
urban-calderon <urbanocalderon@outlook.com>
2025-06-23 18:58:14 -0600

    feat: add a new layout component for the dashboard module

---
+++ src/app/dashboard/layouts/dashboard-layout/dashboard-layout.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'ally-dashboard-layout',
+  standalone: false,
+  templateUrl: './dashboard-layout.component.html',
+  styleUrl: './dashboard-layout.component.css'
+})
+export class DashboardLayoutComponent {
+
+}
