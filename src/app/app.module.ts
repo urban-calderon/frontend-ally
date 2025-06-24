68c2c5a07b29040adf82506e2d6e335fce985438
urban-calderon <urbanocalderon@outlook.com>
2025-06-23 18:56:27 -0600

    feat: add the child modules and define the global paths of the application

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
+
 import { HomeComponent } from './app.component';
 import { SharedModule } from './shared/shared.module';
 import { DashboardModule } from './dashboard/dashboard.module';
