7959efce99970081631cf5e68f7dc80a12f4381e
urban-calderon <urbanocalderon@outlook.com>
2025-06-25 01:35:45 -0600

    feat: add environment variable settings

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,9 @@
+export const environment = {
+  API_URL_BACKEND: 'http://localhost:3000',
+  API_URL_WEATHER: 'http://api.openweathermap.org',
+  API_URL_TIME_ZONE: 'https://api.timezonedb.com',
+
+  API_KEY_WEATHER: '3890a26b0982db6a39ade8c1fd4677ae',
+  API_KEY_TIME_ZONE: '1YW3ZGLW72M5'
+
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,8 @@
+export const environment = {
+  API_URL_BACKEND: 'https://backend-ally.onrender.com',
+  API_URL_WEATHER: 'http://api.openweathermap.org',
+  API_URL_TIME_ZONE: 'https://api.timezonedb.com',
+
+  API_KEY_WEATHER: '3890a26b0982db6a39ade8c1fd4677ae',
+  API_KEY_TIME_ZONE: '1YW3ZGLW72M5'
+};
