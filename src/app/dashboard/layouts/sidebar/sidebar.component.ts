fe4fe6fd30e33c928c239dc60776e21c25c1a473
urban-calderon <urbanocalderon@outlook.com>
2025-06-25 06:18:21 -0600

    style: sidebar to responsive

--- src/app/dashboard/layouts/sidebar/sidebar.component.ts
+++ src/app/dashboard/layouts/sidebar/sidebar.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'ally-sidebar',
@@ -7,5 +7,25 @@ import { Component } from '@angular/core';
   styleUrl: './sidebar.component.css'
 })
 export class SidebarComponent {
+  sidebarActive = false;
 
+  toggleSidebar() {
+    this.sidebarActive = !this.sidebarActive;
+  }
+
+  @HostListener('document:click', ['$event'])
+  onClick(event: MouseEvent) {
+    const target = event.target as HTMLElement;
+    if (this.sidebarActive && !target.closest('.sidebar') && !target.closest('.sidebar-toggle')) {
+      this.sidebarActive = false;
+    }
+  }
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event: Event) {
+    const window = event.target as Window;
+    if (window.innerWidth > 992 && this.sidebarActive) {
+      this.sidebarActive = false;
+    }
+  }
 }
