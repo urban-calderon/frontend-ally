20d6bdcfecd81c3ac674c03d2bbace9f8d8370ef
urban-calderon <urbanocalderon@outlook.com>
2025-06-25 01:47:28 -0600

    feat: add page weather

---
+++ src/app/dashboard/pages/weather/weather.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'ally-page-weather',
+  standalone: false,
+  templateUrl: './weather.component.html',
+  styleUrl: './weather.component.css'
+})
+export class WeatherPageComponent {
+
+}
