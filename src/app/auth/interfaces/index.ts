2492bc1f7009b2df2feb443d1b6679877cc69e1a
urban-calderon <urbanocalderon@outlook.com>
2025-06-25 06:21:10 -0600

    feat: complete the user registration module

--- src/app/auth/interfaces/index.ts
+++ src/app/auth/interfaces/index.ts
@@ -2,3 +2,5 @@ export * from './login-response.interface';
 export * from './user.interface';
 export * from './auth-status.enum';
 export * from './data-response.interface';
+export * from './user-register.interface';
+export * from './register-response.interface';
---
+++ src/app/auth/interfaces/user-register.interface.ts
@@ -0,0 +1,6 @@
+export interface UserRegister {
+  id:        number;
+  email:     string;
+  name:      string;
+  createdAt: Date;
+}
